caa0842ba5f307b06401c0d34be22aa738155310
dibkb <dibas9110@gmail.com>
2024-01-23 23:34:59 +0530

    reverse stack

--- Stack/1.cpp
+++ Stack/1.cpp
@@ -1,32 +1,32 @@
 #include <stack>
 #include <iostream>
 using namespace std;
-void removeElement(stack<int> &st, int k)
-{
-    if(k == 1)
-        return;
-    k--;
-    int temp = st.top();
-    cout << " K " << k;
-    cout << " temp " << temp;
-    st.pop();
-    removeElement(st, k);
-    st.push(temp);
-};
-void solve(stack<int> &st, int k,int num){
-    if(k == 0){
-        st.push(num);
-        return;
-    }
-    int temp = st.top();
-    st.pop();
-    solve(st, k - 1,num);
-    st.push(temp);
-}
-void insertAtBottom(stack<int> &st, int num){
-    int n = st.size();
-    solve(st, n, num);
-};
+// void removeElement(stack<int> &st, int k)
+// {
+//     if(k == 1)
+//         return;
+//     k--;
+//     int temp = st.top();
+//     cout << " K " << k;
+//     cout << " temp " << temp;
+//     st.pop();
+//     removeElement(st, k);
+//     st.push(temp);
+// };
+// void solve(stack<int> &st, int k,int num){
+//     if(k == 0){
+//         st.push(num);
+//         return;
+//     }
+//     int temp = st.top();
+//     st.pop();
+//     solve(st, k - 1,num);
+//     st.push(temp);
+// }
+// void insertAtBottom(stack<int> &st, int num){
+//     int n = st.size();
+//     solve(st, n, num);
+// };
 template <typename T>
 void printStack(const std::stack<T>& myStack) {
     std::stack<T> tempStack = myStack;
@@ -37,6 +37,21 @@ void printStack(const std::stack<T>& myStack) {
 
     std::cout << std::endl;
 }
+void insertBottom(stack<int> &st, int n){
+    if(st.empty()){
+        st.push(n);
+        return;
+    }
+    int temp = st.top();
+    st.pop();
+    insertBottom(st, n);
+    st.push(temp);
+}
+void reverseStack(stack<int> &st){
+    int temp = st.top();
+    st.pop();
+    insertBottom(st, temp);
+};
 using namespace std;
 int main(){
     stack<int> st;
@@ -46,7 +61,7 @@ int main(){
     st.push(40);
     st.push(50);
     printStack(st);
-    insertAtBottom(st,99);
+    reverseStack(st);
     printStack(st);
     // solve(st);
     return 0;
